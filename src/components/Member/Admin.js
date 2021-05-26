8d78cb12198c721ecdac42a659abc1b8dac2d5fb
jing0318 <jing0318@naver.com>
2021-05-26 21:29:10 +0900

    Feat: 회원 조회 구현

--- src/components/Member/Admin.js
+++ src/components/Member/Admin.js
@@ -12,8 +12,8 @@ import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import Popup from '../Common/Popup';
-import MemberForm from './MemberForm';
 import axios from 'axios';
+import { InputBase, Button, Paper } from '@material-ui/core';
 const columns = [
   { id: 'account', label: '아이디', minWidth: '25%' },
   { id: 'nickname', label: '닉네임', miWidth: '25%', align: 'center' },
@@ -46,8 +46,22 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flex: '1 1 100%',
   },
+  thead: { fontWeight: 'bold' },
+  wrapper: {
+    width: '85%',
+    margin: '0 auto',
+    minHeight: '700px',
+  },
+  inputwrapper: {
+    width: '95%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: '2%',
+    paddingTop: '2%',
+  },
   table: {
-    width: '70%',
+    width: '90%',
     margin: '0 auto',
     borderBottom: 'none',
     maxHeight: 440,
@@ -55,6 +69,19 @@ const useStyles = makeStyles((theme) => ({
   },
   pagination: {
     width: '65%',
+  },
+  input: {
+    height: '50px',
+    border: '2px solid #dedede',
+    borderRadius: '25px',
+    marginRight: '0.5%',
+  },
+  placeholderStyle: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
   },
 }));
 
@@ -64,7 +91,12 @@ export default function Admin() {
   const [memberList, setMemberList] = useState([]);
   const [openPopup, setOpenPopup] = useState(false);
   const [recordFordEdit, setRecordFordEdit] = useState(initialForm);
+  const [question, setQuestion] = useState('');
   console.log(recordFordEdit);
+
+  const onChangeQuestion = (e) => {
+    setQuestion(e.target.value);
+  };
 
   const getMemberList = () => {
     axios
@@ -107,55 +139,73 @@ export default function Admin() {
             회원 조회
           </Typography>
         </Toolbar>
-        <TableContainer>
-          <Table size="small" className={classes.table}>
-            <TableHead className={classes.thead}>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth, textWeight: 'bold' }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {memberList.map((row) => {
-                return (
-                  <StyledTableRow
-                    hover
-                    role="button"
-                    onClick={() => openInPopup(row)}
-                    tabIndex={-1}
-                    key={row.code}
-                  >
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
-                        </TableCell>
-                      );
-                    })}
-                  </StyledTableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Paper className={classes.wrapper}>
+          <Toolbar className={classes.inputwrapper}>
+            <InputBase
+              className={classes.input}
+              placeholder="Search…"
+              classes={{
+                input: classes.placeholderStyle,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+              onChange={onChangeQuestion}
+            />
+            <Button
+              variant="contained"
+              color="default"
+              className={classes.button}
+            >
+              검색
+            </Button>
+          </Toolbar>
+          <TableContainer>
+            <Table size="small" className={classes.table}>
+              <TableHead className={classes.thead}>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth, fontWeight: 'bold' }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {memberList.map((row) => {
+                  return (
+                    <StyledTableRow
+                      hover
+                      role="button"
+                      onClick={() => openInPopup(row)}
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {column.format && typeof value === 'number'
+                              ? column.format(value)
+                              : value}
+                          </TableCell>
+                        );
+                      })}
+                    </StyledTableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
       </form>
       <Popup
         openPopup={openPopup}
         setOpenPopup={setOpenPopup}
         recordFordEdit={recordFordEdit}
       />
-      {/* <MemberForm recordFordEdit={recordFordEdit} />
-      </Popup> */}
     </div>
   );
 }
