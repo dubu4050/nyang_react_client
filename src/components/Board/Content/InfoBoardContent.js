0d34fb89eff8d3a9e78aa06b725397e19ad1e39e
jing0318 <jing0318@naver.com>
2021-05-31 03:14:22 +0900

    Design: 검색바 CSS 수정

--- src/components/Board/Content/InfoBoardContent.js
+++ src/components/Board/Content/InfoBoardContent.js
@@ -7,6 +7,7 @@ import {
   Button,
 } from '@material-ui/core';
 import BoardCard from '../../Card/BoardCard';
+import SearchIcon from '@material-ui/icons/Search';
 import CreateIcon from '@material-ui/icons/Create';
 import axios from 'axios';
 
